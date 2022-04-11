3d8ea653f30b2c510bfae012919a3365e020c629
doomed-phobos <the_ekj.2121@outlook.com>
2022-04-10 19:34:22 -0500

    I'll use <filesystem>

--- src/fs.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-namespace utils {
-   std::string get_canonical_path(const std::string& path);
-   bool is_directory(const std::string& path);
-} // namespace utils
