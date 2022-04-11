40cd0655fdb6970371659f474dacdfa10e0af1e2
doomed-phobos <the_ekj.2121@outlook.com>
2022-04-10 19:34:39 -0500

    Added AppException

--- src/app.hpp
+++ src/app.hpp
@@ -1,18 +1,27 @@
 #pragma once
 #include <memory>
+#include <stdexcept>
 
 class AppCliCommands;
+
+class AppException : public std::runtime_error {
+public:
+   AppException(const std::string& msg) :
+      std::runtime_error(msg) {}
+};
 
 class App {
 public:
    App(int argc, char* argv[]);
    ~App();
 
-   bool run();
+   void run();
 private:
    class Output;
    class VerboseOutput;
    class NullOutput;
+
+   void copy_to(const std::string& from, const std::string& to, bool force);
 
    std::unique_ptr<AppCliCommands> m_acc;
    std::unique_ptr<Output> m_out;
