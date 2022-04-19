d34eb17fb01e49d13280f549944f8b06e0c1643a
doomed-phobos <the_ekj.2121@outlook.com>
2022-04-19 10:26:15 -0500

    Changed app_flags

--- src/app_flags.cpp
+++ src/app_flags.cpp
@@ -1,15 +1,17 @@
 #include "src/app_flags.hpp"
 
-AppFlags::AppFlags() :
-   m_flags(0) {}
+namespace app {
+   AppFlags::AppFlags() :
+      m_flags(0) {}
 
-void AppFlags::setFlags(Enum flags) {
-   m_flags = flags;
-}
-void AppFlags::addFlags(Enum flags) {
-   m_flags |= flags;
-}
+   void AppFlags::setFlags(Enum flags) {
+      m_flags = flags;
+   }
+   void AppFlags::addFlags(Enum flags) {
+      m_flags |= flags;
+   }
 
-bool AppFlags::hasFlags(Enum flags) const {
-   return (m_flags & flags) == flags;
-}
+   bool AppFlags::hasFlags(Enum flags) const {
+      return (m_flags & flags) == flags;
+   }
+} // namespace app
--- src/app_flags.hpp
+++ src/app_flags.hpp
@@ -1,22 +1,24 @@
 #pragma once
 #include <cstdint>
 
-class AppFlags {
-public:
-   enum Enum : uint8_t {
-      kNone            = 0,
-      kIncludeAllFiles = 1 << 0,
-      kForce           = 1 << 1,
-      kRecursive       = 1 << 2,
-      kVerbose         = 1 << 3
+namespace app {
+   class AppFlags {
+   public:
+      enum Enum : uint8_t {
+         kNone            = 0,
+         kIncludeAllFiles = 1 << 0,
+         kForce           = 1 << 1,
+         kRecursive       = 1 << 2,
+         kVerbose         = 1 << 3,
+      };
+
+      AppFlags();
+
+      void setFlags(Enum flags);
+      void addFlags(Enum flags);
+
+      bool hasFlags(Enum flags) const;
+   private:
+      unsigned m_flags;
    };
-
-   AppFlags();
-
-   void setFlags(Enum flags);
-   void addFlags(Enum flags);
-
-   bool hasFlags(Enum flags) const;
-private:
-   unsigned m_flags;
-};
+} // namespace app
