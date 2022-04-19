b2151e8ab7b4a0026ffea83bbfddd8fdb8320b01
doomed-phobos <the_ekj.2121@outlook.com>
2022-04-19 11:00:14 -0500

    Soon Cache

---
+++ src/cache.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace app {
+   /*
+   To save cache:
+      MAGIC_NUMBER
+      PACKAGE_NAME
+      DIRECTORY
+      RELATIVES_DIRECTORIES
+   */
+   class Cache { // File to load and save packages
+   public:
+
+   };
+} // namespace app
