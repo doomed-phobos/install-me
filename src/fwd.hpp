0090d72b65fc8695100c52a5ea86c73bcd03d3f6
doomed-phobos <the_ekj.2121@outlook.com>
2022-05-11 11:43:27 -0500

    Added forward declarations

---
+++ src/fwd.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <bits/fs_fwd.h>
+
+namespace fs = std::filesystem;
+
+namespace app {
+   class PackageManager;
+   class AppFlags;
+   class AppCliCommands;
+   class Package;
+} // namespace app
+
+namespace utils {
+   class WFile;
+   class File;
+} // namespace utils
