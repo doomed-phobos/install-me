7bc284ab8e92e949d33e5b0a60cf6df3d77c4dc5
doomed-phobos <the_ekj.2121@outlook.com>
2022-05-11 11:41:23 -0500

    Changed AppFlags
    * Remove unnecessary flags

--- src/app_flags.hpp
+++ src/app_flags.hpp
@@ -6,10 +6,8 @@ namespace app {
    public:
       enum Enum : uint8_t {
          kNone            = 0,
-         kIncludeAllFiles = 1 << 0,
-         kForce           = 1 << 1,
-         kRecursive       = 1 << 2,
-         kVerbose         = 1 << 3,
+         kVerbose         = 1 << 1,
+         kSymLink         = 1 << 2,
       };
 
       AppFlags();
