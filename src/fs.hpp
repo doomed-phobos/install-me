c4e9735a3a306e3f7341fc97b2f4da057d76d677
doomed-phobos <the_ekj.2121@outlook.com>
2022-05-11 11:43:36 -0500

    Changed fs.hpp

--- src/fs.hpp
+++ src/fs.hpp
@@ -2,3 +2,14 @@
 #include <filesystem>
 
 namespace fs = std::filesystem;
+
+namespace utils {
+   static fs::path get_canonical(const fs::path& path) {
+         std::error_code e;
+         auto canonical_path = fs::canonical(path, e);
+         if(e)
+            throw std::runtime_error("'" + path.string() + "': " + e.message());
+
+         return canonical_path;
+      }
+} // namespace utils
