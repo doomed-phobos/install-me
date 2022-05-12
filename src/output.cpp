9b98e7a962a8877009c504dc1d065ccec75636ae
doomed-phobos <the_ekj.2121@outlook.com>
2022-05-12 11:49:10 -0500

    Created "table" class for print tables

--- src/output.cpp
+++ src/output.cpp
@@ -49,3 +49,110 @@ namespace app {
       puts_with_sign(std::cout, 'V', msg);
    }
 } // namespace app
+
+namespace utils {
+   namespace priv {
+      std::ostream& operator<<(std::ostream& out, const center& c) {
+         std::streamsize w = out.width();
+         if(w > c.str.length()) {
+            std::streamsize left = (w + c.str.length()) / 2;
+            out.width(left);
+            out << c.str;
+            out.width(w - left);
+            out << "";
+         } else {
+            out << c.str;
+         }
+
+         return out;
+      }
+   } // namespace priv
+   
+   // =================
+   // Column
+   // =================
+   table::column::column(table& parent, const std::string& title, unsigned padding) :
+      m_parent(parent),
+      m_title(title),
+      m_padding(padding),
+      m_width(m_title.size()) {}
+   
+   void table::column::addItem(const std::string& item) {
+      m_items.push_back(item);
+      m_width = std::max<std::streamsize>(item.size(), m_width);
+      m_parent.m_totalRows = std::max(m_items.size(), m_parent.m_totalRows);
+   }
+
+   std::streamsize table::column::preferredWidth() const {
+      return m_width + padding()*2;
+   }
+
+   std::string table::column::getItem(size_t i) const {
+      if(i >= 0 && i < m_items.size())
+         return m_items[i];
+      return "";
+   }
+
+   // =================
+   // Table
+   // =================
+   table::table(const std::string& title) :
+      m_title(title),
+      m_totalRows(0),
+      m_totalWidth(1) {
+      m_ss << "|";
+   }
+   
+   void table::addColumn(column&& col) {
+      m_ss << std::setw(col.preferredWidth()) << center(col.title()) << "|";
+      m_totalWidth += 1 + col.preferredWidth();
+      m_columns.push_back(std::move(col));
+   }
+
+   std::ostream& operator<<(std::ostream& out, const table& t) {
+      out << std::setfill('-') << std::setw(t.m_totalWidth) << "" << std::endl;
+      out << std::setfill(' ') << "|" << std::setw(t.m_totalWidth-2) << center(t.m_title) << "|\n";
+      out << std::setfill('-') << std::setw(t.m_totalWidth) << "" << std::endl;
+      out << t.m_ss.str() << std::endl;
+      out << std::setfill('-') << std::setw(t.m_totalWidth) << "" << std::endl;
+      
+      out << std::setfill(' ');
+      for(size_t i = 0; i < t.m_totalRows; ++i) {
+         for(const auto& col : t.m_columns) {
+            out << " " << std::setw(col.preferredWidth()) << center(col.getItem(i)) << "";
+         }
+
+         out << "\n";
+      }
+      
+      return out;
+   }
+
+   /*void table::printItems(std::ostream& out) const {
+      out << std::setfill(' ');
+      for(size_t i = 0; i < m_totalRows; ++i) {
+         for(const auto& col : m_columns) {
+            out << " " << std::setw(col->preferredWidth()) << center(col->getItem(i)) << "";
+         }
+         out << "\n";
+      }
+   }
+
+   void table::print(std::ostream& out) const {
+      std::stringstream ss;
+      std::streamsize totalWidth = 1;
+      for(const auto& col : m_columns) {
+         ss << "|" << std::setw(col->preferredWidth()) << center(col->title());
+         totalWidth += 1 + col->preferredWidth();
+      }
+      ss << "|\n";
+      
+      out << std::setfill('-') << std::setw(totalWidth) << "" << std::endl;
+      out << std::setfill(' ') << "|" << std::setw(totalWidth-2) << center(m_title) << "|\n";
+      out << std::setfill('-') << std::setw(totalWidth) << "" << std::endl;
+      out << ss.str();
+      out << std::setfill('-') << std::setw(totalWidth) << "" << std::endl;
+
+      printItems(out);
+   }*/
+} // namespace utils
--- src/output.hpp
+++ src/output.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 namespace app {
    class Output {
@@ -32,7 +33,55 @@ namespace app {
    public:
       virtual void verbose(const std::string& msg) override;
    };
-} // namespace app::out
+} // namespace app
+
+namespace utils {
+   namespace priv {
+      struct center {
+         const std::string_view& str;
+
+         friend std::ostream& operator<<(std::ostream&, const center&);
+      };
+   } // namespace priv
+
+   class table {
+   public:
+      class column {
+      public:
+         column(table& parent, const std::string& table, unsigned offset = 1);
+
+         void addItem(const std::string& item);
+         void setPadding(std::streamsize padding) {m_padding = padding;}
+
+         const std::string& title() const {return m_title;}
+         std::streamsize padding() const {return m_padding;}
+         std::streamsize preferredWidth() const;
+         std::string getItem(size_t i) const;
+      private:
+         friend table;
+
+         table& m_parent;
+         std::vector<std::string> m_items;
+         std::string m_title;
+         std::streamsize m_padding;
+         size_t m_width;
+      };
+
+      table(const std::string& title);
+
+      void addColumn(column&& col);
+   private:
+      friend std::ostream& operator<<(std::ostream& out, const table& t);
+
+      std::string m_title;
+      std::vector<column> m_columns;
+      std::stringstream m_ss;
+      size_t m_totalRows;
+      size_t m_totalWidth;
+   };
+
+   inline priv::center center(const std::string_view& str) {return {str};}
+} // namespace utils
 
 #define INFO(msg) app::Output::Instance()->info(msg)
 #define VERBOSE(msg) app::Output::Instance()->verbose(msg)
