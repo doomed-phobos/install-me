1cc81fd2d8ee5f73de27a97b90ac28df16a67fb1
doomed-phobos <the_ekj.2121@outlook.com>
2022-05-23 20:45:44 -0500

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include "src/app.hpp"
+#include "src/parse_exception.hpp"
 #include "src/output.hpp"
+#include "src/string.hpp"
+#include "src/generate_macros.hpp"
 
 int main(int argc, char* argv[]) {
    try {
       app::App app(argc, argv);
       app.run();
+
+      return 0;
+   } catch(const app::ParseException& e) {
+      ERROR(utils::fmt_to_str("%s\n"
+         "Try '%s -?' or '%s --help' instead for more information.", e.what(), PROJECT_NAME, PROJECT_NAME));
    } catch(const std::runtime_error& e) {
       ERROR(e.what());
-      return 1;
    }
 
-   return 0;
+   return 1;
 }
