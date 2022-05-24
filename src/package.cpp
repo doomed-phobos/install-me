184c16f9fd33342f37d3fc91f8566ed4d7ca6c97
doomed-phobos <the_ekj.2121@outlook.com>
2022-05-23 20:46:47 -0500

    Updated Package

--- src/package.cpp
+++ src/package.cpp
@@ -2,54 +2,21 @@
 
 #include "src/fs.hpp"
 #include "src/output.hpp"
+#include "src/string.hpp"
+#include "src/package_exception.hpp"
 
 namespace app {
-   Package::Package(const fs::path& out, const std::string& pkgName) :
-      m_outDir(out),
-      m_pkgName(pkgName) {}
-
-   Package::Package(const fs::path& in, const fs::path& out, bool symLink) :
-      m_inDir(in),
-      m_outDir(out),
-      m_pkgName(in.stem().string()),
-      m_symLink(symLink) {
-      std::error_code e;
-      if(fs::create_directory(out, e))
-         VERBOSE("Output dir " + m_outDir.string() + " created ...");
-      if(e)
-         throw std::runtime_error("Output dir: " + e.message());
-   }
-
-   void Package::copyFrom(const fs::path& path) {
-      if(fs::is_directory(path))
-         return;
-      
-      auto relativePath = fs::relative(path, m_inDir);
-      if(relativePath.has_parent_path()) {
-         auto newDir = m_outDir/relativePath.parent_path();
-         std::error_code e;
-         fs::create_directories(newDir, e);
-         if(e)
-            throw std::runtime_error("Directory creation '" + newDir.string() + "': " + e.message());
-         VERBOSE("Directory '" + newDir.string() + "' created ...");
-      }
-      std::error_code e;
-      auto outPath = m_outDir/relativePath;
-      fs::copy(path,
-         outPath,
-         m_symLink ? fs::copy_options::create_symlinks : fs::copy_options::none,
-         e);
-      if(e)
-         throw std::runtime_error("Package copying '" + outPath.string() + "': " + e.message());
-
-      addPath(relativePath);
-   }
-
-   void Package::addPath(const fs::path& path) {
+   Package::Package(const fs::path& output, const std::string& pkgName, bool didOutputExists) :
+      m_outDir(output),
+      m_name(pkgName),
+      m_didOutputExists(didOutputExists) {}
+   
+   void Package::addRelativePath(const fs::path& path) {
       m_paths.push_back(path);
    }
 
-   const std::string& Package::pkgName() const {return m_pkgName;}
+   const std::string& Package::name() const {return m_name;}
    const fs::path& Package::outDir() const {return m_outDir;}
    const Package::PathList& Package::relativePaths() const {return m_paths;}
+   bool Package::didOutputExists() const {return m_didOutputExists;}
 } // namespace app
--- src/package.hpp
+++ src/package.hpp
@@ -2,30 +2,59 @@
 #include "src/fs.hpp"
 
 #include <vector>
-#include <memory>
 
 namespace app {
    class Package {
    public:
       typedef std::vector<fs::path> PathList;
+      template<typename Base>
+      class absolute_path_iterator {
+      public:
+         typedef std::forward_iterator_tag     iterator_category;
+         typedef std::ptrdiff_t                difference_type;
+         typedef fs::path                      value_type;
+         typedef typename Base::const_iterator const_base_iterator;
 
-      Package(const fs::path& in, const fs::path& out, bool symLink);
+         absolute_path_iterator(const_base_iterator end) :
+            m_it(end) {}
+         absolute_path_iterator(const fs::path& output, const_base_iterator beg) :
+            m_output(output),
+            m_it(beg) {}
 
-      void copyFrom(const fs::path& path);
+         value_type operator*() const {
+            return m_output/(*m_it);
+         }
 
-      const std::string& pkgName() const;
+         absolute_path_iterator& operator++() {
+            m_it++;
+            return *this;
+         }
+
+         bool operator!=(const absolute_path_iterator& that) const {
+            return m_it != that.m_it;
+         }
+      private:
+         fs::path m_output;
+         const_base_iterator m_it;
+      };
+
+      void addRelativePath(const fs::path& path);
+
+      const std::string& name() const;
       const fs::path& outDir() const;
       const PathList& relativePaths() const;
+
+      auto begin() const {return absolute_path_iterator<PathList>(m_outDir, m_paths.begin());}
+      auto end() const {return absolute_path_iterator<PathList>(m_paths.end());}
    private:
       friend class PackageManager;
-      
-      Package(const fs::path& out, const std::string& pkgName);
-      void addPath(const fs::path& path);
-   private:
-      bool m_symLink;
-      fs::path m_inDir;
+
+      Package(const fs::path& output, const std::string& pkgName, bool didOutputExists);
+      bool didOutputExists() const;
+
+      bool m_didOutputExists;
       fs::path m_outDir;
       PathList m_paths;
-      std::string m_pkgName;
+      std::string m_name;
    };
 } // namespace app
